5cefe8e9489450e97426c1a101303e53527fbeca
Thomas Chaplin <tomrchaplin@gmail.com>
2019-08-13 17:43:30 +0100

    Starting proportions display work

--- app/graphics.js
+++ app/graphics.js
@@ -1,3 +1,12 @@
+const removeOldSquares = () => {
+	squareIDList.forEach( id => {
+		oldSquare = document.getElementById(id);
+		if(oldSquare) {
+			oldSquare.parentNode.removeChild(oldSquare);
+		}
+	});
+}
+
 const addSquare = (placement, squareClass, squareID, text) => {
 	document.getElementById("rating_square_"+placement).innerHTML += `<div class='rating_placement ${squareClass}' id='${squareID}'>${text}</div>`
 }
@@ -14,19 +23,30 @@ const showSquares = (periodName, factorFunction) => {
 	inherentPlacement = severityLikelihoodToPlacement(inherentSeverity, inherentLikelihood);
 	addSquare(inherentPlacement, `${periodName}_square`, `${periodName}_ir_square`, 'IR');
 }
-const removeOldSquares = () => {
-	squareIDList.forEach( id => {
-		oldSquare = document.getElementById(id);
-		if(oldSquare) {
-			oldSquare.parentNode.removeChild(oldSquare);
+
+const showProportions = (factorFunction) => {
+	allFactors = factorFunction(true);
+	proportionList = factorsToProportions(allFactors);
+	irfPropHolder = document.getElementById("irf_proportions");
+	controlPropHolder = document.getElementById("control_proportions");
+	irfHTML = "";
+	controlHTML = "";
+	proportionList.forEach( prop => {
+		if(prop.is_control){
+			controlHTML += `<div>${prop.label} ::: ${Math.round(prop.proportion)}</div>`;
+		} else {
+			irfHTML += `<div>${prop.label} ::: ${Math.round(prop.proportion)}</div>`;
 		}
-	});
+	})
+	irfPropHolder.innerHTML = irfHTML;
+	controlPropHolder.innerHTML = controlHTML;
 }
 
 const updateView = () => {
 	removeOldSquares();
 	if(document.getElementById("whatif_checkbox").checked) {
 		showSquares('whatif', getWhatIfFactors);
+		showProportions(getWhatIfFactors);
 	}
 	if(document.getElementById("current_checkbox").checked) {
 		showSquares('current', getCurrentFactors);
